41c3a95c71f3b68f7e1c3fed8b1c460435721fb7
uroojishere <urooj Fatima>
2025-07-25 05:16:07 +0500

    Wipe dev branch and replace with latest project

--- src/data/getColleges.js
+++
@@ -1,8 +0,0 @@
-import supabase from "@/lib/supabase";
-export const getColleges = async () => {
-    const { data, error } = await supabase.from("colleges").select("*");
-    if (error) {
-        throw new Error(error.message);
-    }
-    return data;
-};
